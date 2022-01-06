10adaa81fe667eb8a1c05a36e6f199aea6a33c0e
Luis Hernandez <abdalaluis2@gmail.com>
2022-01-06 12:50:24 +0000

    second commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Image from "next/Image";
 import Link from "next/link";
 import { getCategories } from "../services";
 
@@ -13,23 +12,26 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-12 mb-6">
-      <div className="border-b w-full inline-block border-sky-400">
+    <div className="container mx-auto px-12 mb-8">
+      <div className="border-b w-full inline-block border-gray-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <Image
-              className="cursor-pointer"
+            <div className="cursor-pointer font-bold text-4xl text-red-600">
+              <span>luisYjho</span>
+            </div>
+            {/* <Image
+              className="cursor-pointer font-bold text-4xl text-green-600"
               src="/../public/logo.png"
               alt="luisYjho logo"
-              width={180}
-              height={170}
-            />
+              width={120}
+              height={100}
+            /> */}
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category, index) => (
             <Link key={index} href={`/category/${category.slug}`}>
-              <span className="caveat md:float-right mt-4  text-red-600 hover:text-green-400 ml-4  cursor-pointer">
+              <span className="md:float-right mt-2 align-middle text-red-600 hover:text-green-400 ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
             </Link>
