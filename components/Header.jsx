531ba0d7e31f92ca8f006be508e3886fab5d836c
Luis Hernandez <abdalaluis2@gmail.com>
2022-01-07 12:16:11 +0000

    fourth commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,22 +14,14 @@ const Header = () => {
 
   return (
     <div className="container mx-auto px-12 mb-8">
-      <div className="border-b w-full inline-block border-blue-200 py-8">
+      <div className="border-b w-full inline-block border-yellow-600 py-8">
         <div className="md:float-left font-bold text-4xl text-red-600">
           <Link href="/">
-            <div>
-              <div>
-                <Image
-                  className="cursor-pointer"
-                  src="/../public/logo.png"
-                  alt="luisYjho logo"
-                  width={140}
-                  height={110}
-                  inline
-                />
-              </div>
-            </div>
+            <span className="cursor-pointer inline caveat_large text-red-600 ">
+              luis<p className="caveat_large text-green-600 inline">Y</p>jho
+            </span>
           </Link>
+          <p className="text-3xl inline text-yellow-600"> Blog</p>
         </div>
 
         <div className="hidden md:float-left md:contents">
@@ -40,10 +32,6 @@ const Header = () => {
               </span>
             </Link>
           ))}
-          <span className="inline caveat_large text-red-600 ">
-            luis<p className="caveat_large text-green-600 inline">Y</p>jho
-          </span>
-          <p className="text-3xl inline text-yellow-600"> Blog</p>
         </div>
       </div>
     </div>
